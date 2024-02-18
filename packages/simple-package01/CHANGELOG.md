a50a39c1c97c73b0d1b18fc6a8dca87d91278dfc
Rodrigo <rodrigo.medeiros@grupoboticario.com.br>
2024-02-18 17:14:28 -0300

    Atualizado package01

---
+++ packages/simple-package01/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @brtancik/simple-package01
+
+## 0.0.1
+
+### Patch Changes
+
+- Removido função de print no console
--- packages/simple-package01/app.js
+++ packages/simple-package01/app.js
@@ -1,3 +1,3 @@
 // app.js
 console.log("Hello, world, package 01!");
-console.log("Update package 01!");
+// console.log("Update package 01!");
